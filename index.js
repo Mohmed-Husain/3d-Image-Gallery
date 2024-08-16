75f0b27eb05792cb84a624c007c6790ce5c71565
[Mohmed-Husain] <[arkonafoob@gmail.com]>
2024-08-16 11:37:00 +0530

    Auto Rotator added

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-const imageContainerE1 =document.querySelector(".image-container")
+    const imageContainerE1 =document.querySelector(".image-container")
 
-const PrevE1 = document.getElementById("Prev")
-const NextE1 = document.getElementById("Next")
+    const PrevE1 = document.getElementById("Prev")
+    const NextE1 = document.getElementById("Next")
 
-let x=0
-PrevE1.addEventListener("click",()=>{
-    x= x + 45 ;
-    updateGallery()
-})
+    let x=0;
+    let timer;
+    
+    function stopAutoRotate() {
+        
+        if (timer) {
+            clearInterval(timer);
+        }
+        
+        
+        timer = setInterval(() => {
+            x = x - 45; 
+            updateGallery();
+        }, 3000);
+    }
+    
+    function updateGallery(){
+        imageContainerE1.style.transform=`perspective(1000px) rotateY(${x}deg)`;
 
-NextE1.addEventListener("click",()=>{
-    x= x - 45 ;
-    updateGallery()
-})
+    }
+
+    PrevE1.addEventListener("click",()=>{
+        x= x + 45 ;
+        
+        updateGallery()
+        stopAutoRotate()
+    })
+
+    NextE1.addEventListener("click",()=>{
+        x= x - 45 ;
+        
+        updateGallery()
+        stopAutoRotate()
+    })
 
 
-function updateGallery(){
-    imageContainerE1.style.transform=`perspective(1000px) rotateY(${x}deg)`;
-}
+stopAutoRotate()
+
 
